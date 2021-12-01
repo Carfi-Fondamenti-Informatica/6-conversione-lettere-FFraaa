5e22961a8c34484395a5c70e938e93f87f017453
FFraaa <91596644+FFraaa@users.noreply.github.com>
2021-12-01 23:15:23 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,15 +1,18 @@
 #include "lib.h"
 bool conversione (char &a){
-  bool t=true;
-  if (a>=65 & a<=90 or a>=97 & a<=122){
-    t=true;
-    if(a>= 65 & a<=90){
-      a=a+32
-        return &a;
+    bool t =true;
+    if (a>=65 & a<=90 or a>=97 & a<=122){
+        t=true;
+        if(a>=65 & a<=90){
+            a=a+32;
+            return &a;
+        }else{
+            a=a-32;
+            return &a;
+        }
     }else{
-      a=a-32
-        return &a;
+        t=false;
     }
-  }else{
-    t=false;}
-  return t;
+    return t;
+}
+
