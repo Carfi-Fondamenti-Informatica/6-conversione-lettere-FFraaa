e782443a64d9aa3b4f9a68e8f03686e6954c8adb
FFraaa <91596644+FFraaa@users.noreply.github.com>
2021-12-01 10:45:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  char a=a;
+  cin>>a;
+  if(conversione(&: a)== false){
+    cout<<"errore"<<endl;
+  }else{
+    cout<<a<<endl;
+  }
   return 0;
 }
